8960febd6eeceb4c205e6bd95ef30d806a565aaf
Jonah Brüchert <jbbgameich@gmail.com>
2018-02-04 13:54:55 +0100

    Add workaround for non-complete image urls in Bhushan Shah's blog

--- build.sh
+++ build.sh
@@ -2,4 +2,12 @@
 
 pluto update planet.ini
 ruby -r 'jekyll/planet' -e 'JekyllPlanet.main'
+
+# Workaround for non-complete image urls in Bhushan Shah's blog
+for post in _posts/*; do
+	if grep "author:     Bhushan Shah" $post >/dev/null; then
+		sed -i -e 's_/images_http://blog.bshah.in/images_g' $post
+	fi
+done
+
 jekyll build
