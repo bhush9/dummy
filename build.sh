a25556325ba73fea1f9fe8088a9a2970688d84a7
Jonah Brüchert <jbbgameich@gmail.com>
2018-02-05 16:41:26 +0100

    build.sh: Make workaround indentation independent

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ ruby -r 'jekyll/planet' -e 'JekyllPlanet.main'
 
 # Workaround for non-complete image urls in Bhushan Shah's blog
 for post in _posts/*; do
-	if grep "author:     Bhushan Shah" $post >/dev/null; then
+	if grep "author:"  $post | grep "Bhushan Shah" >/dev/null; then
 		sed -i -e 's_/images_http://blog.bshah.in/images_g' $post
 	fi
 done
